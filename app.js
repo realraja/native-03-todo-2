64d4a1130b762a0b298793e33afbed724cdc5cf9
raja_choudhary <raja@choudhary.com>
2024-05-22 23:26:09 +0530

    set for netlify

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import  User  from './routers/User.js';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
-import cors from 'cors'
+import cors from 'cors';
+import serverless from 'serverless-http'
 
 export const app = express();
+const router = express.Router();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +16,8 @@ app.use(fileUpload({
   useTempFiles: true,
 }))
 app.use(cors());
-
+app.use('/netlify/functions/api',router)
+module.exports.handler = serverless(app);
 app.use("/api/v1",User); 
 
 app.get("/", (req, res) => {
